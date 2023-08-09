7b73ec93eabf5023309501323ca3674565ff3fd1
cole <bernankeic@gmail.com>
2023-08-09 13:52:06 +0800

    fix(sfc): exclude test files from `dist`

--- template/sfc/build.config.ts
+++ template/sfc/build.config.ts
@@ -2,8 +2,8 @@ import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
   entries: [
-    { builder: "mkdist", input: "./src/" },
-    { builder: "mkdist", input: "./src/", format: "cjs", ext: "js" },
+    { builder: "mkdist", input: "./src/", pattern: ["**", "!test"] },
+    { builder: "mkdist", input: "./src/", format: "cjs", ext: "js", pattern: ["**", "!test"] },
   ],
   declaration: true,
   clean: true,
