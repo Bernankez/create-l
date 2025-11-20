d6e497b8072ccdeda0f2923de015c05d261db4fc
cole <bernankeic@gmail.com>
2025-11-20 18:13:49 +0800

    fix: tsup output file extension

--- templates/tsup/tsup.config.ts
+++ templates/tsup/tsup.config.ts
@@ -5,6 +5,23 @@ export default defineConfig({
     'src/cli.ts',
     'src/index.ts',
   ],
+  outExtension({ format }) {
+    let js: string;
+    switch (format) {
+      case "cjs":
+        js = ".cjs";
+        break;
+      case "esm":
+        js = ".mjs";
+        break;
+      case "iife":
+        js = ".global.js";
+        break;
+    }
+    return {
+      js,
+    };
+  },
   shims: true,
   inject: ['cjs-shims.ts'],
 })
